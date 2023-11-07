3bc7c859ae3aae6ecf40bf06582dfff48fe989f8
adityash1 <aditya167411@gmail.com>
2023-11-07 12:40:23 +0530

    feat: left sidebar for large devices

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "@/components/shared/Navbar";
+import { LeftSidebar } from "@/components/shared/search/LeftSidebar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -7,7 +8,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <main className="background-light850_dark100 relative">
         <Navbar />
         <div className="flex">
-          LeftSidebar
+          <LeftSidebar />
           <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
             <div className="mx-auto w-full max-w-5xl">{children}</div>
             RightSidebar
