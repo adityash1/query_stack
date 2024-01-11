0defa1dc248dd455535f119bd404073845f870fc
adityash1 <aditya167411@gmail.com>
2024-01-12 00:16:14 +0530

    feat: webhook integration with clerk auth

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,9 @@
 
 import { connectToDatabase } from "@/lib/mongoose";
 import User from "@/database/user.model";
+import {CreateUserParams, DeleteUserParams, UpdateUserParams} from "@/lib/actions/shared.types";
+import {revalidatePath} from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -14,3 +17,56 @@ export async function getUserById(params: any) {
     throw error;
   }
 }
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+      await connectToDatabase();
+      const newUser = await User.create(userData);
+      return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+      await connectToDatabase();
+      const {clerkId, updateData, path} = params;
+      await  User.findOneAndUpdate({clerkId}, updateData, {
+          new: true,
+      })
+
+      revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+    try {
+        await connectToDatabase();
+        const {clerkId} = params;
+        const {user} = await  User.findOneAndDelete({clerkId});
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        // Delete user from database. everything created by them.
+        const userQuestionIds = await Question.find({author: user._id}).distinct('_id');
+
+        // delete their questions
+        await Question.deleteMany({author: user._id});
+
+        // TODO: delete answers, comments, etc.
+
+        const deletedUser = await User.findByIdAndDelete(user._id);
+
+        return deletedUser;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
