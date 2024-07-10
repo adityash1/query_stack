7cb16e12b8556d529243cae9ff280dde7c843c0a
Aditya Sharma <aditya167411@gmail.com>
2024-07-10 23:59:26 +0530

    filter state

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -37,8 +37,6 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
         />
       </div>
 
-      {/* <HomeFilters /> */}
-
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
           result.questions.map((question: any) => (
--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,15 +1,44 @@
+"use client";
+
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "recommended";
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active !== item) {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive("");
+      const newUrl = removeKeysFromQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["filter"],
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
 
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
         <Button
           key={item.value}
-          // onClick={() => {}}
+          onClickCapture={() => handleTypeClick(item.value)}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active === item.value
               ? "bg-primary-100 text-primary-500"
